08dd7abf43334873f3006a6e1e2337c880723011
Marceille Eventz <156130198+ventzDev509@users.noreply.github.com>
2024-08-08 09:30:30 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   // app.useGlobalPipes(new ValidationPipe());
   const PORT=process.env.PORT || 4000;
   const cors={
-    origin: [`http://localhost:${PORT}`],
+    origin: [`https://nestprojet.onrender.com:${PORT}`],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   }
   app.enableCors(cors);
